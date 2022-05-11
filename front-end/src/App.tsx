757aa9037bfedb84cde7abd54d5b522910e091d7
jademichellepeche <jademichellepeche@gmail.com>
2022-05-11 09:22:49 +0200

    Added PendingApprovalsPage

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import UploadActivityPage from './pages/UploadActivityPage/UploadActivityPage';
+import PendingApprovalsPage from './pages/PendingApprovalsPage/PendingApprovalsPage';
 
 
 setupIonicReact();
@@ -42,6 +43,7 @@ const App: React.FC = () => (
           <UploadActivityPage></UploadActivityPage>
         </Route>
         <Route exact path='/PendingApprovals'>
+          <PendingApprovalsPage></PendingApprovalsPage>
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
