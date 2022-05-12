6b85fbc9d0e717fed47e44ce2c532a030c5c2d7a
jademichellepeche <jademichellepeche@gmail.com>
2022-05-12 22:42:17 +0200

    Update Unit Tests

--- front-end/src/components/approvalButton/approvalButton.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import ApprovalButton from './approvalButton'
-
-/*
-test('renders without crashing', () => {
-    const { baseElement } = render(<ApprovalButton userID={0} username='username' />);
-    expect(baseElement).toBeDefined();
-  });
-*/
