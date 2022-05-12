f7b847dba6db37c625aa045785eb7b7e9a3d976f
Trunks29 <u17169811@tuks.co.za>
2022-05-12 18:25:52 +0200

    use geolocator for single search

--- front-end/src/pages/AddGym/AddGym.tsx
+++ front-end/src/pages/AddGym/AddGym.tsx
@@ -4,41 +4,39 @@ import { ToolBar } from '../../components/toolbar/Toolbar'
 import { useState } from 'react';
 import { Map, Overlay} from "pigeon-maps";
 import { stamenToner } from 'pigeon-maps/providers'
+import Geocoder from "react-native-geocoding";
 const AddGym: React.FC = () => {
 
  
   const image:string="https://www.pngfind.com/pngs/m/219-2197153_gym-building-sport-training-svg-png-free-.png"
 
-  const queryString=window.location.search
-  const urlParams = new URLSearchParams(queryString);
-  var name=urlParams.get('name')
+  const queryString:string=window.location.search
+  const urlParams:any = new URLSearchParams(queryString);
+  var name:string=urlParams.get('name')
   if(!name)
   { name="name"}
   const [gymName,setGymName]=useState<string>(name);
-  var address=urlParams.get('address')
+  var address:string=urlParams.get('address')
   if(!address)
   { address="address"}
   const [gymAddress,setGymAddress]=useState<string>(address);
 
-  var y:number
-  var x:number
-  var latitude=urlParams.get('latitude')
-  if(!latitude)
-  { y=-25.7545 }
-  else
-  { y=parseFloat(latitude) }
-  var longitude=urlParams.get('longitude')
-  if(!longitude)
-  { x= 28.2314 }
-  else
-  { x=parseFloat(longitude)}
-
-
-  const center:[number,number]=[y,x]
-  const zoom:number =19
-  const href:string="http://localhost:3000/AddGymLocation?name="+gymName+"&address="+gymAddress+"&latitude="+y+"&longitude="+x
+  const [latitude,setlatitude]=useState<number>(-25.7545);
+  var y:number=urlParams.get('latitude')
+  if(y)
+  { setlatitude(y) }
   
-  let gymIcon="logo";
+  const [longitude,setlongitude]=useState<number>(28.2314);
+  var x:number=urlParams.get('longitude')
+  if(x)
+  { setlongitude(x) }
+
+
+  const center:[number,number]=[latitude,longitude]
+  const zoom:number =16
+  const href:string="http://localhost:3000/AddGymLocation?name="+gymName+"&address="+gymAddress+"&latitude="+latitude+"&longitude="+longitude
+  
+  let gymIcon:string="logo";
   const addGym=()=>{
       fetch(`https://gym-king.herokuapp.com/gyms/gym?gbn=${gymName}&ga=${gymAddress}&gclo=${x}&gcla=${y}&gi=${gymIcon}`,{
           "method":"POST"
@@ -49,7 +47,23 @@ const AddGym: React.FC = () => {
       })
       .catch(err => {console.log(err)})
     }
-
+    const handleKeyDown = (event: { key: string; }) => {
+      if (event.key === 'Enter') {
+        console.log("using geocoding api")
+        Geocoder.init("AIzaSyD9pQDwcGJFK6NRGNj5-YwdJBx2PtERCTg")
+        Geocoder.from(gymAddress)
+        .then(json => {
+        var addressComponent = json.results[0].geometry.location;
+           console.log(addressComponent);
+           setlatitude(addressComponent.lat)
+           setlongitude(addressComponent.lng)
+           
+         })
+        .catch(error => console.warn(error));
+        
+      }
+    }
+   
   return (
     <IonPage>
       <IonHeader>
@@ -80,7 +94,7 @@ const AddGym: React.FC = () => {
               </IonRow>
 
               <IonRow className='left'>
-                <IonInput class="AddGymInput" placeholder='address' value={gymAddress} onIonChange={(e:any)=>{setGymAddress(e.target.value);}}> </IonInput>
+                <IonInput class="AddGymInput" placeholder='address' value={gymAddress} onKeyDown={handleKeyDown} onIonChange={(e:any)=>{setGymAddress(e.target.value);}}> </IonInput>
               </IonRow>
 
               <IonRow  className='left topMargin'>
@@ -90,7 +104,7 @@ const AddGym: React.FC = () => {
               <IonRow className='left'>
                 <IonButton expand="block" class="flex-margin" href={href}>
                   <IonIcon class="AddGymLocation" name="location-outline"></IonIcon>
-                    <span>Change Co-ordinates</span>
+                    <span>adjust Co-ordinates</span>
                    <IonIcon class="AddGymArrow" name="chevron-forward-outline"></IonIcon>
                 </IonButton>
               </IonRow>
@@ -101,7 +115,7 @@ const AddGym: React.FC = () => {
                   zoom={zoom}
                   provider={stamenToner}
                   > 
-                  <Overlay anchor={[y,x]} offset={[30,30]} >
+                  <Overlay anchor={[latitude,longitude]} offset={[30,30]} >
                       <img width={60} height={50} src={image} alt="builing icon">
                       </img> 
                     </Overlay>
@@ -120,3 +134,5 @@ const AddGym: React.FC = () => {
 };
 
 export default AddGym;
+
+
