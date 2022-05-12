d47284df6e99750a028fe9f3490bfe3b1b90b151
jademichellepeche <jademichellepeche@gmail.com>
2022-05-12 12:00:06 +0200

    Added validation to UploadActivity

--- front-end/src/components/activityInputs/ActivityInputs.tsx
+++ front-end/src/components/activityInputs/ActivityInputs.tsx
@@ -1,15 +1,35 @@
 import {IonGrid, IonInput, IonRow, IonText, IonItem, IonList} from '@ionic/react';
-import React from 'react'
+import React, { useState } from 'react'
 
 
 //creating a type so props can be entered
 
-export type props = {activityCategory: string};
+export type props = {activityCategory: string, inputs:any};
+export type states = {i1:string, i2:string, i3:string}
 
-export class  ActivityInputs extends React.Component<props>{
+export class  ActivityInputs extends React.Component<props, states>{
+    
+    activityStates: states = {i1:'', i2:'', i3:''}
+    
+
+    handleChange = (e:any) =>{
+        let input = e.target.name;
+        let value = e.target.value;
+        if(input == 'i1')
+            this.activityStates.i1=value;
+        else if(input == 'i2')
+            this.activityStates.i2=value;
+        else if (input=='i3')
+            this.activityStates.i3=value;
+    }
+    
+
     render(){
+        
+
         if(this.props.activityCategory === 'cardio'){
             return(
+
                 <IonList>
                     <IonItem>
                         <IonGrid className='centerLeft grid'>
@@ -17,7 +37,8 @@ export class  ActivityInputs extends React.Component<props>{
                                 <IonText className='Subheading'>Duration:</IonText>
                             </IonRow>
                         </IonGrid>
-                        <IonInput type='text' className='textInput'></IonInput>
+                        <IonInput name='i1' type='text' className='textInput' value={this.activityStates.i1} onIonChange={this.props.inputs && this.handleChange} ></IonInput>
+                        
                     </IonItem>
                     <br></br>
                     <IonItem>
@@ -26,7 +47,7 @@ export class  ActivityInputs extends React.Component<props>{
                                 <IonText className='Subheading'>Distance:</IonText>
                             </IonRow>
                         </IonGrid>
-                        <IonInput type='text' className='textInput'></IonInput>
+                        <IonInput name='i2' type='text' className='textInput' value={this.activityStates.i2} onIonChange={this.props.inputs && this.handleChange}></IonInput>
                     </IonItem>
                     <br></br>
                     <IonItem>
@@ -35,7 +56,7 @@ export class  ActivityInputs extends React.Component<props>{
                                 <IonText className='Subheading'>Level of Difficulty:</IonText>
                             </IonRow>
                         </IonGrid>
-                        <IonInput type='text' className='textInput' ></IonInput>
+                        <IonInput name='i3' type='text' className='textInput' value={this.activityStates.i3} onIonChange={this.props.inputs && this.handleChange}></IonInput>
                     </IonItem>
                 </IonList>
             )
@@ -49,7 +70,7 @@ export class  ActivityInputs extends React.Component<props>{
                                 <IonText className='Subheading'>Weight:</IonText>
                             </IonRow>
                         </IonGrid>
-                        <IonInput type='text' className='textInput'></IonInput>
+                        <IonInput name='i1' type='text' className='textInput' value={this.activityStates.i1} onIonChange={this.props.inputs && this.handleChange}></IonInput>
                     </IonItem>
                     <IonItem>
                         <IonGrid className='centerLeft grid'>
@@ -57,7 +78,7 @@ export class  ActivityInputs extends React.Component<props>{
                                 <IonText className='Subheading'>Sets:</IonText>
                             </IonRow>
                         </IonGrid>
-                        <IonInput type='text' className='textInput' ></IonInput>
+                        <IonInput name='i2' type='text' className='textInput' value={this.activityStates.i2} onIonChange={this.props.inputs && this.handleChange}></IonInput>
                     </IonItem>
                     <IonItem>
                         <IonGrid className='centerLeft grid'>
@@ -65,7 +86,8 @@ export class  ActivityInputs extends React.Component<props>{
                                 <IonText className='Subheading'>Reps:</IonText>
                             </IonRow>
                         </IonGrid>
-                        <IonInput type='text' className='textInput'></IonInput>
+                        <IonInput name='i3' type='text' className='textInput' value={this.activityStates.i3} onIonChange={this.props.inputs && this.handleChange}></IonInput>
+                        
                     </IonItem>
                 </IonList>
             )
--- front-end/src/pages/UploadActivityPage/UploadActivityPage.tsx
+++ front-end/src/pages/UploadActivityPage/UploadActivityPage.tsx
@@ -1,35 +1,34 @@
 import {IonContent, IonText, IonPage, IonHeader, IonGrid, IonRow, IonButton, IonIcon} from '@ionic/react';
-import React from 'react'
+import React, { useState } from 'react'
 import FileChooser from '../../components/filechooser/FileChooser';
 import { ToolBar } from '../../components/toolbar/Toolbar';
 import {shieldOutline} from 'ionicons/icons';
 import './UploadActivityPage.css';
 import {ActivityInputs} from '../../components/activityInputs/ActivityInputs';
+import {claimSchema} from '../../validation/UploadClaimValidation'
+
 export type UploadActivityStates = {act?:any}
 
-export class UploadActivityPage extends React.Component<UploadActivityStates>{
-
-    badgeId= 1;
-    badgeInfo:any;
-
+const UploadActivityPage: React.FC = () =>{
+    
+    
+    let badgeId= 1;
+    let  badgeInfo = null;
     //GET REQUEST:
-    getBadges=()=>{
-        fetch(`https://gym-king.herokuapp.com/badges/badge?bid=${this.badgeId}`,{
+    const getBadges=()=>{
+        fetch(`https://gym-king.herokuapp.com/badges/badge?bid=${badgeId}`,{
             "method":"GET"
         })
         .then(response =>response.json())
         .then(response =>{
             console.log(response);
-            this.badgeInfo= response;
+            badgeInfo= response;
         })
         .catch(err => {console.log(err)})
     }
-    
-
-    state= {activity:'none'};
-    
+        
     //fake generic badge for testing 
-    badge1 = {
+    const badge1 = {
         name:'Cycling Champ', 
         description: 'Cycle 10km in 15 minutes to earn this badge!', 
         activityCategory:'cardio',
@@ -39,20 +38,62 @@ export class UploadActivityPage extends React.Component<UploadActivityStates>{
         input3:'4'
     };
 
-    /*
-        CARDIO:
-            input1: duration
-            input2: distance
-            input#: level of difficulty
 
-        STRENGTH:
-            input1: weight
-            input2: sets
-            input3: reps
-    */
+
+    //submit claim 
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [input3, setInput3] = useState('');
+    const [isValid, setIsValid] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+
+    localStorage.setItem( 'e1', "");
+    localStorage.setItem( 'e2', "");
+    localStorage.setItem( 'e3', "");
+
+
+   const handleSubmit = async (e:any) =>{
+        e.preventDefault();
+        let formData={
+            i1: e.target.i1.value,
+            i2: e.target.i2.value,
+            i3: e.target.i3.value,
+        };
+        console.log(formData);
+
+        if(formData.i1 == null)
+            localStorage.setItem( 'e1', "This field is required");
+        if(formData.i2 == null)
+            localStorage.setItem( 'e2', "This field is required");
+        if(formData.i3 == null)
+            localStorage.setItem( 'e3', "This field is required");
+
+        const isValid = await claimSchema.isValid(formData);
+        setSubmitted(true);
+        if(isValid)
+        {
+            setIsValid(true);
+            //handle post request 
+        }
+        
+   }
     
-    render(){
+    const updateInputs = (e:any) =>{
+        let input = e.target.name;
+        let value = e.target.value; 
+        if(input == 'i1')
+            setInput1(input1 + value);
+        else if(input == 'i2')
+            setInput2(value);
+        else if (input=='i3')
+            setInput3(value); 
+
+            console.log(input1,input2,input3);
+    }
+    
+    
         return(
+            
             <IonPage color='#220FE' >
                 <IonHeader>
                     <ToolBar></ToolBar>
@@ -60,24 +101,27 @@ export class UploadActivityPage extends React.Component<UploadActivityStates>{
                 <br></br>
                 <IonContent fullscreen className='Content'>
                     <IonIcon icon={shieldOutline} className='badge center shadow'></IonIcon>
-                    <IonText className='PageTitle center'>{this.badge1.name}</IonText>
-                    <IonText className='SmallDescription center'>{this.badge1.description}</IonText> <br></br>
-                    <IonText className='inputHeading'>Enter your activity details:</IonText>
-                    <ActivityInputs activityCategory={this.badge1.activityCategory}></ActivityInputs> <br></br>
-            
-                    <IonGrid className='centerLeft grid'>
-                        <IonRow className='left topMargin'>
-                            <IonText className='Subheading'>Proof</IonText>
-                        </IonRow>
-                    </IonGrid>
-                    <FileChooser numFiles={0}></FileChooser>
+                    <IonText className='PageTitle center'>{badge1.name}</IonText>
+                    <IonText className='SmallDescription center'>{badge1.description}</IonText> <br></br>
+                    <form onSubmit={handleSubmit}>
+                        <IonText className='inputHeading'>Enter your activity details:</IonText>
+                        <ActivityInputs activityCategory={badge1.activityCategory} inputs={updateInputs}></ActivityInputs> <br></br>
+                        {
+                            !isValid && submitted && <IonText className='inputError'>Please enter the required fields</IonText>
+                        }
+                        <IonGrid className='centerLeft grid'>
+                            <IonRow className='left topMargin'>
+                                <IonText className='Subheading'>Proof</IonText>
+                            </IonRow>
+                        </IonGrid>
+                        <FileChooser numFiles={0}></FileChooser>
 
-                    <IonButton class="btnSubmit">SUBMIT</IonButton>
+                        <IonButton class="btnSubmit" type='submit'>SUBMIT</IonButton>
+                    </form>
                 </IonContent>
             </IonPage>
         )
         
-    }
 }
 
 export default UploadActivityPage;
