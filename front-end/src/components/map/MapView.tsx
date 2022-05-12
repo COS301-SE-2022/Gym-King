3c471842f8e3f23f7769647120bb110438eac06e
Dylan Whiteford <93826822+DYL-WF@users.noreply.github.com>
2022-05-12 13:23:54 +0200

    :sparkles: adds hardcoded map elements and onclick events to the map elements

--- front-end/src/components/map/MapView.tsx
+++ front-end/src/components/map/MapView.tsx
@@ -1,9 +1,10 @@
 import { IonButton, IonLoading, IonToast } from "@ionic/react";
-import React, { useState } from "react"
-import { Geolocation } from '@ionic-native/geolocation'
-import { Map ,Overlay} from 'pigeon-maps'
-import { stamenToner } from 'pigeon-maps/providers'
+import React, { useState } from "react";
+import { Geolocation } from '@ionic-native/geolocation';
+import { Map ,Overlay} from 'pigeon-maps';
+import { stamenToner } from 'pigeon-maps/providers';
 
+import './MapView.css';
 interface LocationError {
     showError: boolean;
     message?: String;
@@ -28,16 +29,21 @@ const MapView: React.FC = () =>{
             setCenter([position?.coords.latitude, position?.coords.longitude]) 
             
             setError({showError: false, message: "no error here"})
-            setZoom(18) 
+            setZoom(15) 
 
         } catch(e){
             setLoading(false);
             
-            setError({showError: true, message: "Cannot get user location: Check Permissions"});
+            setError({showError: true, message: "Cannot get userlocation: Check Permissions"});
         }
     }
     const [userLocation, setUserLoc] = useState([0,0])
 
+    const gymButtonClick=()=>{
+        console.log("CLICKED")
+        window.alert("The Gyms Menu will open Up");
+
+    }
     return (
         
         <>
@@ -72,6 +78,34 @@ const MapView: React.FC = () =>{
                     <Overlay anchor={[userLocation[0],userLocation[1]]} offset={[30,30]} >
                     <img src='https://icons-for-free.com/iconfiles/png/512/svg+location+locator+map+navigation+user+user+location+icon-1320184910707394703.png' width={50} height={50} alt='' />
                     </Overlay>      
+
+                    <Overlay 
+                        anchor={[userLocation[0]*0.9995,userLocation[1]*0.9997]} 
+                        offset={[30,30]} 
+                        
+                        >
+                            
+                            <img onClick={gymButtonClick} id = "GymPicture" src='https://www.seekpng.com/png/full/309-3093415_gym-building-sport-training-svg-png-icon-free.png' width={50} height={50} alt='' />
+                    </Overlay> 
+
+                    <Overlay 
+                        anchor={[userLocation[0]*0.9998,userLocation[1]*0.9994]} 
+                        offset={[30,30]} 
+                        
+                        >
+                            
+                            <img onClick={gymButtonClick} id = "GymPicture" src='https://www.seekpng.com/png/full/309-3093415_gym-building-sport-training-svg-png-icon-free.png' width={50} height={50} alt='' />
+                    </Overlay> 
+
+                    <Overlay 
+                        anchor={[userLocation[0]*1.00025,userLocation[1]*1.00015]} 
+                        offset={[30,30]} 
+                        
+                        >
+                            
+                            <img onClick={gymButtonClick} id = "GymPicture" src='https://www.seekpng.com/png/full/309-3093415_gym-building-sport-training-svg-png-icon-free.png' width={50} height={50} alt='' />
+                    </Overlay> 
+
                 </Map>
                 
                 
