a83d4960b7714a3842dc457634bb830635aefe04
jademichellepeche <jademichellepeche@gmail.com>
2022-05-30 10:08:12 +0200

    Renamed files and folders for consistency

--- front-end/src/components/toolbar/Toolbar.test.tsx
+++ front-end/src/components/toolbar/Toolbar.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Toolbar from './Toolbar'
+import Toolbar from './Toolbar';
 
 test('renders without crashing', () => {
     const { baseElement } = render(<Toolbar username='user'/>);
