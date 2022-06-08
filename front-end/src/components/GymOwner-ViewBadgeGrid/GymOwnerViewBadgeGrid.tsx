adab3f7ee7f8ee29b21b86ffd89f28141a22527f
Trunks29 <u17169811@tuks.co.za>
2022-06-08 02:16:38 +0200

    add View badge grid

---
+++ front-end/src/components/GymOwner-ViewBadgeGrid/GymOwnerViewBadgeGrid.tsx
@@ -0,0 +1,39 @@
+import { IonCol, IonGrid, IonRow} from '@ionic/react';
+import { useEffect, useState } from 'react';
+import ViewBadgeCard from '../ViewBadgeCard/ViewBadgeCard';
+
+export const GymOwnerViewBadgeGrid=(props: {gymID:string})=>{
+     
+    const [badges, setBadges] = useState(new Array<any>());
+    
+        //GET REQUEST:
+        useEffect(()=>
+        {
+            fetch(`https://gym-king.herokuapp.com/badges/gym?gid=${props.gymID}`,{
+                "method":"GET"
+            })
+            .then(response =>response.json())
+            .then(response =>{
+                console.log("this is the response")
+                console.log(response)
+                setBadges(response.results)
+            })
+            .catch(err => {console.log(err)})
+        },[])
+    return(
+        <IonGrid>
+        <IonRow  className="ion-align-items-start">
+        {badges.map(el => 
+        
+            <IonCol  key={el.b_id}>
+                <ViewBadgeCard  BadgeID={el.b_id} BadgeTitle={el.badgename} BadgeDesc={el.badgedescription} BadgeImg={0}></ViewBadgeCard>
+             </IonCol>)}
+        </IonRow>
+
+    </IonGrid>
+        )
+        
+    
+}
+
+export default GymOwnerViewBadgeGrid;
