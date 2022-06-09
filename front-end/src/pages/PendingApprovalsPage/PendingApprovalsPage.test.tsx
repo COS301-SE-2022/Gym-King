4f44c4a8065c81b17fbef8334120a0886b7e2951
jademichellepeche <jademichellepeche@gmail.com>
2022-06-09 20:11:47 +0200

    Added Unit Tests

--- front-end/src/pages/PendingApprovalsPage/PendingApprovalsPage.test.tsx
+++ front-end/src/pages/PendingApprovalsPage/PendingApprovalsPage.test.tsx
@@ -5,3 +5,25 @@ test('renders without crashing', () => {
     const { baseElement } = render(<PendingApprovalsPage />);
     expect(baseElement).toBeDefined();
 });
+
+
+describe('Testing API calls', () => {
+
+    it('should load pending claims data', async () => {
+        (()=>{
+            let gymId="lttD"
+            fetch(`https://gym-king.herokuapp.com/claims/claim?gid=${gymId}`,{
+                "method":"GET"
+            })
+            .then(response =>response.json())
+            .then(response =>{
+                expect (response).toBeDefined
+            })
+            .catch(err => {
+                expect (err).toBeDefined
+            })
+        })
+    });
+
+
+})
--- front-end/src/pages/UploadActivityPage/UploadActivityPage.test.tsx
+++ front-end/src/pages/UploadActivityPage/UploadActivityPage.test.tsx
@@ -7,4 +7,43 @@ test('renders without crashing', () => {
   });
 
 
-  //when the image can't be loaded, a defauly image must be displayed 
+  describe('Testing API calls', () => {
+
+    it('should load badge data', async () => {
+      (()=>{
+        let badgeId='XRQ'
+        fetch(`https://gym-king.herokuapp.com/badges/badge?bid=${badgeId}`,{
+            "method":"GET"
+        })
+        .then(response =>response.json())
+        .then(response =>{
+            expect (response).toBeDefined()
+        })
+        .catch(err => {
+            expect (err).toBeDefined()
+        })
+      } )
+    });
+
+    it('should load send claim data', async () => {
+        (()=>{
+          let b_id="XRQ"
+          let email=""
+          let username = ""
+          let i1=""
+          let i2= ""
+          let i3= ""
+          fetch(`https://gym-king.herokuapp.com/claims/claim?bid=${b_id}&email=${email}&username=${username}&input1=${i1}&input2=${i2}&input3=${i3}&proof=${'PROOF'}`,{
+              "method":"POST"
+          })
+          .then(response =>response.json())
+          .then(response =>{
+              expect (response).toBeDefined()
+          })
+          .catch(err => {
+              expect (err).toBeDefined()
+          }) 
+       });
+      });
+
+  })
