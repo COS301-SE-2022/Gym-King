7a7d999378963611cbce2cc8c8dea9665ebd4e7b
Trunks29 <u17169811@tuks.co.za>
2022-07-11 21:22:59 +0200

    add actionsheet
    action sheet presents user with opportunity to edit or delete a gym

--- front-end/src/components/GymCard/GymCard.tsx
+++ front-end/src/components/GymCard/GymCard.tsx
@@ -1,12 +1,16 @@
-import { IonCard, IonCardSubtitle, IonCardTitle} from '@ionic/react';
+import { IonActionSheet, IonCard, IonCardSubtitle, IonCardTitle} from '@ionic/react';
+import { caretForwardCircle, create, heart, share, trash } from 'ionicons/icons';
+import { useState } from 'react';
 import './GymCard.css'
 
 export const GymCard=(prop:{id:any,name:string,address:string})=>{
-   
+    const [showActionSheet, setShowActionSheet] = useState(false);
     return(
+        <div>
             <IonCard
                 color="primary"   
                 class="ion-padding"
+                onClick={() => setShowActionSheet(true)}
             >
                   <IonCardTitle>
                     {prop.name}
@@ -15,6 +19,36 @@ export const GymCard=(prop:{id:any,name:string,address:string})=>{
                     {prop.address}
                   </IonCardSubtitle>
             </IonCard>
+            <IonActionSheet
+                isOpen={showActionSheet}
+                onDidDismiss={()=>setShowActionSheet(false)}
+                cssClass="my-custom-class"
+                buttons={[{
+                    text: 'Edit',
+                    icon: create,
+                    handler: () => {
+                      console.log('edit clicked');
+                    }
+                  },{
+                    text: 'Delete',
+                    role: 'destructive',
+                    icon: trash,
+                    id: 'delete-button',
+                    data: {
+                      type: 'delete'
+                    },
+                    handler: () => {
+                      console.log('Delete clicked');
+                    }
+                  },{ 
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                      console.log('Cancel clicked');
+                    }
+                  }]}>
+            </IonActionSheet>
+        </div>
         )
         
     
