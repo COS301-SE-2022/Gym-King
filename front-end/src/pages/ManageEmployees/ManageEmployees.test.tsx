a85ded607cf3031e4fbc564fb24f9b93b2b30c99
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-07-20 12:36:04 +0200

    Fixed typing error

--- front-end/src/pages/ManageEmployees/ManageEmployees.test.tsx
+++ front-end/src/pages/ManageEmployees/ManageEmployees.test.tsx
@@ -1,5 +1,5 @@
 import {fireEvent, render, screen} from '@testing-library/react';
-import ManageEmployees from '/ManageEmployees';
+import ManageEmployees from './ManageEmployees';
 
 ////TESTS TO BE PERFORMED////
 /*
