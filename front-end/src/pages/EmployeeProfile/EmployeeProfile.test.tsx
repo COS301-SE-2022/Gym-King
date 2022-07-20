13ed20631edeb14c3206568068ba72d20570ac18
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-07-20 12:22:30 +0200

    Added basic test from EmployeeProfile

---
+++ front-end/src/pages/EmployeeProfile/EmployeeProfile.test.tsx
@@ -0,0 +1,11 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import EmployeeProfile from '/EmployeeProfile';
+
+////TESTS TO BE PERFORMED////
+/*
+*/
+
+test('renders without crashing', () => {
+  const {baseElement } = render(</EmployeeProfile/>);
+  expect(baseElement).toBeDefined();
+});
