69033698633741df11956345feaf89e9d4024c51
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-07-20 12:29:52 +0200

    fixed small errors

--- front-end/src/pages/EmployeeProfile/EmployeeProfile.test.tsx
+++ front-end/src/pages/EmployeeProfile/EmployeeProfile.test.tsx
@@ -1,11 +1,11 @@
 import {fireEvent, render, screen} from '@testing-library/react';
-import EmployeeProfile from '/EmployeeProfile';
+import EmployeeProfilePage from '/EmployeeProfile';
 
 ////TESTS TO BE PERFORMED////
 /*
 */
 
 test('renders without crashing', () => {
-  const {baseElement } = render(</EmployeeProfile/>);
+  const {baseElement } = render(<EmployeeProfile/>);
   expect(baseElement).toBeDefined();
 });
