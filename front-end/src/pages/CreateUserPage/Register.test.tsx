d28ca789978fc2d259e5778acf1d862c0e72fe9e
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-07-20 14:03:16 +0200

    Update Register.test.tsx

--- front-end/src/pages/CreateUserPage/Register.test.tsx
+++ front-end/src/pages/CreateUserPage/Register.test.tsx
@@ -1,11 +1,11 @@
 import {fireEvent, render, screen} from '@testing-library/react';
-import RegisterPage from './CreateUserPage';
+import Register from './CreateUserPage';
 
 ////TESTS TO BE PERFORMED////
 /*
 */
 
 test('renders without crashing', () => {
-  const {baseElement} = render(<Register />);
+  const {baseElement} = render(<RegisterPage />);
   expect(baseElement).toBeDefined();
 });
