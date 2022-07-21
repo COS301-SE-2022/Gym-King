d2e95e1b2281d62bbfc4021f0b649220b831de64
Trunks29 <u17169811@tuks.co.za>
2022-07-21 16:09:40 +0200

    add delete gym functionality

--- front-end/src/components/GymCard/GymCard.tsx
+++ front-end/src/components/GymCard/GymCard.tsx
@@ -1,13 +1,52 @@
-import { IonActionSheet, IonCard, IonCardSubtitle, IonCardTitle, useIonAlert} from '@ionic/react';
+import { IonActionSheet, IonCard, IonCardSubtitle, IonCardTitle, IonLoading, IonToast, useIonAlert} from '@ionic/react';
 import { create, trash } from 'ionicons/icons';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './GymCard.css'
 
 export const GymCard=(prop:{id:any,name:string,address:string})=>{
+    const [loading, setLoading] = useState<boolean>(false);
+    const [showToast1, setShowToast1] = useState(false);
+    const [showToast2, setShowToast2] = useState(false);
+    const [toastMessage,setToastMessage]=useState("could not delete gym")
     const history=useHistory();
     const [showActionSheet, setShowActionSheet] = useState(false);
     const [presentAlert] = useIonAlert();
+    const deleteGym = (password:string) => {
+    setLoading(true)
+    fetch(`https://gym-king.herokuapp.com/owner/delete/gym`,
+    {
+      method: "DELETE",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        gid:prop.id,
+        "email":localStorage.getItem("email"),
+        "password":password,
+      })
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      setLoading(false)
+      console.log(response)
+      if(response?.success===false)
+      {
+        setToastMessage(response.results)
+        setShowToast2(true)
+      }
+      else{
+        setShowToast1(true)
+      }
+    })
+    .catch((err) => {
+      setLoading(false)
+      console.log(err);
+      setShowToast2(true)
+    });
+    }
     return(
         <div>
             <IonCard
@@ -50,11 +89,17 @@ export const GymCard=(prop:{id:any,name:string,address:string})=>{
                         header:'delete:',
                         subHeader:prop.name,
                         message:'delete cannot be undone',
+                        inputs:[
+                          {
+                            name: 'password',
+                            type:'password',
+                            placeholder:'password'
+                          }],
                         buttons:[
                             {
                                 text:'YES',
                                 cssClass:"alert-button-yes",
-                                handler:()=>{console.log("yes")}
+                                handler:(alertData)=>{deleteGym(alertData.password);}
                             },
                             {
                                 text:'NO',
@@ -73,6 +118,29 @@ export const GymCard=(prop:{id:any,name:string,address:string})=>{
                     }
                   }]}>
             </IonActionSheet>
+            <IonToast
+        isOpen={showToast1}
+        onDidDismiss={() => setShowToast1(false)}
+        message="Gym has been deleted."
+        duration={500}
+        color="success"
+      />
+      <IonToast
+        isOpen={showToast2}
+        onDidDismiss={() => setShowToast2(false)}
+        message={toastMessage}
+        duration={500}
+        color="danger"
+      />
+      <IonLoading 
+        isOpen={loading}
+        message={"Loading"}
+        duration={2000}
+        spinner={"circles"}
+        onDidDismiss={() => setLoading(false)}
+        cssClass={"spinner"}
+        
+      />
         </div>
         )
         
