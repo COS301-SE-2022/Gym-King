5ef5053bcfa8aee02d5dcb55db9903d86d8fac85
Trunks29 <u17169811@tuks.co.za>
2022-07-21 14:26:51 +0200

    add api to update and delete gym

--- back-end/src/repositories/gym.repository.ts
+++ back-end/src/repositories/gym.repository.ts
@@ -21,6 +21,9 @@ export const gymRepository = GymKingDataSource.getRepository(gym).extend({
         newGym.gym_icon = icon;
         return this.manager.save(newGym);
     },
+    async updateGym(gid: string, brandname: string, address: string, lat: number, long: number, icon: string) {
+        return await this.manager.update(gym, {g_id: gid}, {gym_brandname: brandname, gym_address: address, gym_coord_lat: lat, gym_coord_long: long, gym_icon: icon})
+    },
     deleteGym(gid: string) {
         return this.manager.delete(gym, {g_id: gid})
     }
