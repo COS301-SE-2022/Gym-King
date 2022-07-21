f5ad2f116f9fd77e9e9daabee5df943b6150956d
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-07-21 10:06:13 +0200

    Update Register.test.tsx

--- front-end/src/pages/CreateUserPage/Register.test.tsx
+++ front-end/src/pages/CreateUserPage/Register.test.tsx
@@ -1,5 +1,5 @@
 import {fireEvent, render, screen} from '@testing-library/react';
-import {RegisterPage} from './CreateUserPage';
+import {register} from './CreateUserPage';
 
 ////TESTS TO BE PERFORMED////
 /*
