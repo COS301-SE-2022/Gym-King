a86b9ffe99259da7342447519ade86f7f0212cd4
jademichellepeche <jademichellepeche@gmail.com>
2022-07-25 22:06:34 +0200

    Added ActivityInputs unit test

--- front-end/src/components/activityInputs/ActivityInputs.test.tsx
+++ front-end/src/components/activityInputs/ActivityInputs.test.tsx
@@ -12,22 +12,21 @@ test('renders without crashing', () => {
   - test that activtystates update when value in input boxes change (test handle change function)
 
 */
-/*
+
 
 describe('Testing prop text values', () => {
 
   test('correctly displays cardio activitytype inputs', async () => {
-    const {baseElement} = render(<ActivityInputs activityCategory='CARDIO' inputs={{input1:'00:23:00', input2:'3', input3:'4'}} />);
+    const {baseElement} = render(<ActivityInputs activityCategory='cardio' inputs={{input1:'00:23:00', input2:'3', input3:'4'}} />);
     expect (baseElement).toHaveTextContent("Duration");
     expect (baseElement).toHaveTextContent("Distance");
     expect (baseElement).toHaveTextContent("Level of Difficulty");
   }); 
-  test('correctly displays cardio activitytype inputs', async () => {
-    const {baseElement} = render(<ActivityInputs activityCategory='STRENGTH' inputs={{input1:'00:23:00', input2:'3', input3:'4'}} />);
+  test('correctly displays strength activitytype inputs', async () => {
+    const {baseElement} = render(<ActivityInputs activityCategory='strength' inputs={{input1:'00:23:00', input2:'3', input3:'4'}} />);
     expect (baseElement).toHaveTextContent("Weight");
     expect (baseElement).toHaveTextContent("Sets");
     expect (baseElement).toHaveTextContent("Reps");
   }); 
 });
 
-*/
