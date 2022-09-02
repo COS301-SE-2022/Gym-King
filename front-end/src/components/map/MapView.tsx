cd383c04e2e271f5a7d40dbdbb4918a283ffaa77
Dylan Whiteford <whiteford.dyl@gmail.com>
2022-09-02 18:20:19 +0200

    ⬆️ changing geolocation package from ionic native to capacitor

--- front-end/src/components/map/MapView.tsx
+++ front-end/src/components/map/MapView.tsx
@@ -1,6 +1,6 @@
 import { createAnimation, IonButton,  IonButtons,  IonCard,  IonCardContent,  IonCardHeader,  IonCardTitle,  IonContent,  IonLoading, IonModal, IonToast } from "@ionic/react";
 import React, { useEffect, useState } from "react";
-import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation } from '@capacitor/geolocation';
 import { Map ,Overlay} from 'pigeon-maps';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
@@ -167,17 +167,7 @@ const MapView: React.FC = () =>{
     })
 
     const [showModal, setShowModal] = useState(false);
-    // const getPermissons = () => {
-    //     AndroidPermissions.checkPermission(AndroidPermissions.PERMISSION.LOCATION_HARDWARE).then(
-    //         result => {console.log('Has permission?',result.hasPermission)},
-    //         err =>{ AndroidPermissions.requestPermission(AndroidPermissions.PERMISSION.LOCATION_HARDWARE)},
-            
-    //       );
-          
-    //       AndroidPermissions.requestPermissions([AndroidPermissions.PERMISSION.LOCATION_HARDWARE, AndroidPermissions.PERMISSION.GET_ACCOUNTS]);
-          
-    // }
-    // useIonViewWillEnter(getPermissons)
+
    
     const enterAnimation = (baseEl: any) => {
         const root = baseEl.shadowRoot;
