98200a891889a7bd2c5ddf6cef62944bf8ca37dd
jademichellepeche <jademichellepeche@gmail.com>
2022-09-14 13:13:03 +0200

    Added password policy and form validation

--- front-end/src/components/Register/Password.tsx
+++ front-end/src/components/Register/Password.tsx
@@ -1,39 +1,90 @@
-import {IonButton, IonCol, IonGrid, IonInput, IonRow, IonText} from '@ionic/react';
-import React from 'react'
+import {IonButton, IonCol, IonGrid, IonInput, IonLabel, IonRow, IonText} from '@ionic/react';
+import React, { useState } from 'react'
 import '../../theme/variables.css'
+import { validPassword, matchingPasswords } from '../../utils/validation';
+
 //creating a type so props can be entered
 export type props = { handleChange:any, next:any, prev:any};
 
-export class Password extends React.Component<props>{
+export const Password: React.FC<props> = (props) =>{
 
-    continue = (e:any) => {
+    const [errors, setErrors] = useState({
+        password: '',
+        confirmPassword: '',
+    });
+    
+    const handleError = (error:string, input:string) => {
+        setErrors(prevState => ({...prevState, [input]: error}));
+    };
+    const  validate = () => {
+        let isValid = true;
+        let password = sessionStorage.getItem('regPassword')
+        let confirmPassword = sessionStorage.getItem('confirmPassword')
+
+        if(password && !validPassword(password)) {
+            handleError('Must be at least 8 characters with at least  1 uppercase, lowercase, number and symbol.', 'password');
+            isValid = false;
+        }
+        else
+            handleError('', 'password');
+    
+        if(confirmPassword && password && !matchingPasswords(confirmPassword, password)) {
+            handleError('Passwords don\'t match.', 'confirmPassword');
+            isValid = false;
+        }
+        else
+            handleError('', 'surname');
+
+
+
+        return isValid;
+    }
+
+    const next = (e:any) => {
         e.preventDefault();
         sessionStorage.setItem('regPassword', e.target.pwd.value)
         sessionStorage.setItem('confirmPassword', e.target.confirmPwd.value)
-        this.props.next();
+        
+        let isValid = validate()
+        
+        if(isValid)
+        {
+            props.next();
+        }
     };
 
-    prev = (e:any)=> {
+    const prev = (e:any)=> {
         e.preventDefault();
-        this.props.prev();
+        props.prev();
     };
 
-    render(){
         return(
-            <form className='registerForm' onSubmit={this.continue}>
+            <form className='registerForm' onSubmit={next}>
                 <IonText className='center inputHeading'>Register</IonText>
                 <br></br>
 
                 <IonText className="smallHeading">Password*</IonText>
-                <IonInput name='pwd' type='password' className='textInput' required  value={sessionStorage.getItem('regPassword')!}></IonInput><br></br>
+                <IonInput name='pwd' type='password' className='textInput' required  value={sessionStorage.getItem('regPassword')!}></IonInput>
+                {errors.password!=="" && (
+                    <>
+                    <IonLabel className="errText" style={{"color":"darkorange"}}>{errors.password}</IonLabel><br></br>
+                    </>
+                )}
+                <br></br>
 
                 <IonText className="smallHeading">Confirm Password*</IonText>
-                <IonInput name='confirmPwd' type='text' className='textInput' required  value={sessionStorage.getItem('confirmPassword')!}></IonInput><br></br>
+                <IonInput name='confirmPwd' type='password' className='textInput' required  value={sessionStorage.getItem('confirmPassword')!}></IonInput>
+                {errors.confirmPassword!=="" && (
+                    <>
+                    <IonLabel className="errText" style={{"color":"darkorange"}}>{errors.confirmPassword}</IonLabel><br></br>
+                    </>
+                )}
+                <br></br>
 
                 <IonGrid>
                     <IonRow>
                         <IonCol size='6'>
-                            <IonButton color="primary" className=" btnLogin ion-margin-top" style={{"width":"100%", "margin":"0"}} onClick={this.prev}  >Previous</IonButton>
+                            <IonButton color="primary" className=" btnLogin ion-margin-top" style={{"width":"100%", "margin":"0"}} onClick={prev}  >Previous</IonButton>
                         </IonCol>
                         <IonCol size='6'>
                             <IonButton color="warning" className=" btnLogin ion-margin-top" type="submit" style={{"width":"100%", "margin":"0"}}>Next</IonButton>
@@ -43,7 +94,6 @@ export class Password extends React.Component<props>{
             </form>
         )
         
-    }
 }
 
 export default Password;
--- front-end/src/utils/validation.tsx
+++ front-end/src/utils/validation.tsx
@@ -13,3 +13,11 @@ export const validEmail = (str:string)=>{
 export const validPhone = (str:string)=>{
     return str.length===10
 }
+
+export const validPassword = (str:string)=>{
+    return /(?=.{8,})(?=.*?[^\w\s])(?=.*?[0-9])(?=.*?[A-Z]).*?[a-z].*/.test(str)
+}
+
+export const matchingPasswords = (str1:string, str2:string)=>{
+    return str1===str2
+}
