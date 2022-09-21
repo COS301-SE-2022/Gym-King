cc3603642f4fdd76204cc88bd4cbe001ec3f3b2c
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-09-21 14:02:17 +0200

    Update GymSearchBar.test.tsx

--- front-end/src/components/GymSearchBar/GymSearchBar.test.tsx
+++ front-end/src/components/GymSearchBar/GymSearchBar.test.tsx
@@ -2,7 +2,8 @@ import {render} from '@testing-library/react';
 import {GymSearchBar} from './GymSearchBar'
 
 test('renders without crashing', () => {
-  const {baseElement} = render(<GymSearchBar gyms={[]}, nearByCallback(), searchCallBack, setGymFocus/>);
+ // const {baseElement} = render(<GymSearchBar gyms={[]}, nearByCallback(), searchCallBack, setGymFocus/>);
+  const {baseElement} = render(<GymSearchBar={[]}/>);
   expect(baseElement).toBeDefined();
 });
 
