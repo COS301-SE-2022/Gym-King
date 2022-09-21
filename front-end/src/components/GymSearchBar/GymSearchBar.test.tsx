26496b1fe00044eb7a7cb803a13e80f983e27e84
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-09-21 13:46:38 +0200

    Created Unit Test for Gym Search Bar

---
+++ front-end/src/components/GymSearchBar/GymSearchBar.test.tsx
@@ -0,0 +1,12 @@
+import {render} from '@testing-library/react';
+import {GymSearchBar} from './GymSearchBar'
+
+test('renders without crashing', () => {
+  const {baseElement} = render(<GymSearchBar/>);
+  expect(baseElement).toBeDefined();
+});
+
+////TESTS TO BE PERFORMED ////
+/*
+
+*/
