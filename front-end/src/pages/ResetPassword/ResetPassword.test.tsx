9e5e8699cc1545f863db15a8e460e05caed035d7
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-09-22 16:38:37 +0200

    Create ResetPassword.test.tsx

---
+++ front-end/src/pages/ResetPassword/ResetPassword.test.tsx
@@ -0,0 +1,43 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import ResetPassword from './ResetPassword';
+
+test('renders without crashing', () => {
+  const {baseElement} = render(<ResetPassword />);
+  expect(baseElement).toBeDefined();
+});
+
+test('correctly displays labels', async () => {
+  const {baseElement} =  render(<ResetPassword />);
+  expect(baseElement).toHaveTextContent("New Password");
+  expect(baseElement).toHaveTextContent("Confirm New Password");
+});
+
+
+//////INTEGRATION TESTS //////////
+
+describe('Testing connection to API', () => {
+
+  it('should reset a User Password', async () => {
+     (async () => {
+        fetch(process.env["REACT_APP_GYM_KING_API"]+'/users/user/password', {
+           method: 'PUT',
+           headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+           },
+           data:{
+                email: sessionStorage.getItem("enteredEmail"),
+                otp: sessionStorage.getItem("enteredOTP"),
+                newpassword: sessionStorage.getItem("enteredPassword"),
+           } 
+        })
+        .then(response =>response.data)
+        .then(response => {
+            expect(response).toBeDefined()
+        })
+        .catch(err => {
+            expect(err).toBeDefined()
+        })
+     })
+  });
+})
