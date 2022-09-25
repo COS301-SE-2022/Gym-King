d8cd3907575c9bfd67533729671741e51ed10ee8
jademichellepeche <jademichellepeche@gmail.com>
2022-09-25 21:18:02 +0200

    Fixed unit test

--- front-end/src/pages/AddGym/AddGym.test.tsx
+++ front-end/src/pages/AddGym/AddGym.test.tsx
@@ -2,14 +2,14 @@ import {render,screen} from '@testing-library/react';
 import AddGym from './AddGym';
 /*UNIT TESTING*/
 
-/*
+
 //test if pages rendered
 test('renders without crashing', () => {
   const { baseElement } = render(<AddGym/>);
   expect(baseElement).toBeDefined();
   const linkElement = screen.getByText(/Add Gym/i);
   expect(linkElement).toBeInTheDocument();
-});
+});/*
 
 test('correctly displays labels', async () => {
   const {baseElement} = render(<AddGym/>);
