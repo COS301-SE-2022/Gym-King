0517382ea8cf8b5431f9924c0afd88da2dd3c572
jademichellepeche <jademichellepeche@gmail.com>
2022-09-26 20:16:03 +0200

    Fixed GymList

---
+++ front-end/src/components/GymsList/GymItem/GymItem.tsx
@@ -0,0 +1,58 @@
+import { IonItem, IonList, IonAvatar, IonImg, IonLabel, useIonViewWillEnter} from '@ionic/react';
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import { useHistory } from 'react-router-dom';
+
+export type props = {gymId?:any}
+
+const GymsList: React.FC<props> = (props) =>{
+
+    let history=useHistory()
+
+        const [gymBrandName, setGymBrandName] = useState("")
+        const [gymName, setGymName] = useState("")
+        const [gymAddress, setGymAddress] = useState("")
+        const [gymProfile, setGymProfile] = useState("")
+        useEffect(()=>{
+            //get gym info 
+             axios.get(process.env["REACT_APP_GYM_KING_API"]+`/gyms/gym/${props.gymId}`)
+            .then(response =>response.data)
+            .then(response =>{
+                console.log(response)
+                setGymBrandName(response.gym_brandname)
+                setGymName(response.gym_name)
+                setGymAddress(response.gym_address)
+                setGymProfile("")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        })
+        
+
+        const viewGymProfile = (brandname:string, address:string) =>{
+            sessionStorage.setItem("gid", props.gymId);
+            sessionStorage.setItem("gym_brandname", brandname);
+            sessionStorage.setItem("gym_address", address);
+            history.push("/GymPage")
+        }
+
+        return(
+
+            <IonItem button detail  onClick={()=>viewGymProfile(gymBrandName, gymAddress)} key={props.gymId+ Math.random()}>
+                    <IonAvatar style={{"marginRight":"1em", "marginBottom":"3%"}}>
+                        <IonImg  style={{"position":"absolute","overflow":"hidden","marginTop":"6px","borderRadius":"50%","backgroundImage":`url(${gymProfile})`}} alt="" className="toolbarImage  contain "  ></IonImg>                        
+                    </IonAvatar>
+                    <IonLabel>{gymName}</IonLabel>
+            </IonItem>)
+                    
+                
+        
+               
+        
+        
+
+}
+
+export default GymsList;
+
--- front-end/src/components/GymsList/GymsList.tsx
+++ front-end/src/components/GymsList/GymsList.tsx
@@ -1,84 +1,30 @@
-import { IonItem, IonList, IonAvatar, IonImg, IonLabel, useIonViewDidEnter} from '@ionic/react';
+import { IonItem, IonList, IonAvatar, IonImg, IonLabel, useIonViewWillEnter} from '@ionic/react';
 import React, {useState} from 'react'
 import axios from "axios";
+import { useHistory } from 'react-router-dom';
+import GymItem from './GymItem/GymItem';
 
 export type props = {gymsList?:any}
 
 const GymsList: React.FC<props> = (props) =>{
 
-    let objectArray:Object[]=[]
-    const [gyms, setGyms] = useState(objectArray)
-    useIonViewDidEnter(async()=>{
-        let array: any[]=[]
-        let gymIDs: any[]=[]
-        let gymObjects: any[]=[]
-        await axios(process.env["REACT_APP_GYM_KING_API"]+`/users/user/getGymSubscriptions`,{
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            },
-            data: JSON.stringify({ 
-                fromEmail: localStorage.getItem("email"),
+    let history=useHistory()
 
-            })
-        })
-        .then(response =>response.data)
-        .then(response =>{
-            console.log(response)
-            array = response.results
-        })
-        .catch(err => {
-            console.log(err)
-            
-            
-        })
-        //console.log(array)
-        //extract the gymsIDs from the results 
-        // eslint-disable-next-line 
-        array.map((el)=>{
-            gymIDs.push(el.toGym)
-        })
-        //console.log(gymIDs)
-        //get the gym information for each g_id
-
-        // eslint-disable-next-line 
-        gymIDs.map(async (gid)=>{
-            let gym ={}
-            await axios.get(process.env["REACT_APP_GYM_KING_API"]+`/gyms/gym/${gid}`)
-            .then(response =>response.data)
-            .then(response =>{
-                console.log(response)
-                gym = response
-            })
-            .catch(err => {
-                console.log(err)
-            })
-
-            gymObjects.push(gym)
-        })
-        console.log(gymObjects)
-        setGyms(gymObjects)
-        console.log(gymObjects)
         
-    },[])
 
-
-        const viewGymProfile = () =>{
-
+        const viewGymProfile = (el:any) =>{
+            sessionStorage.setItem("gid", el.g_id);
+            sessionStorage.setItem("gym_brandname", el.gym_brandname);
+            sessionStorage.setItem("gym_address", el.gym_address);
+            history.push("/GymPage")
         }
 
         return(
             
             <IonList>
                 {
-                    gyms.map((el:any)=>{
-                        return (<IonItem button detail  onClick={viewGymProfile} data-testid="aB" key={el.g_id + Math.random()}>
-                                <IonAvatar style={{"marginRight":"1em", "marginBottom":"3%"}}>
-                                    <IonImg  style={{"position":"absolute","overflow":"hidden","marginTop":"6px","borderRadius":"50%","backgroundImage":`url(${el.profile})`}} alt="" className="toolbarImage  contain "  ></IonImg>                        
-                                </IonAvatar>
-                                <IonLabel>{el.gym_name}</IonLabel>
-                            </IonItem>)
+                    props.gymsList?.map((el:any)=>{
+                        return (<GymItem gymId={el.toGym}></GymItem>)
                     })
                 }
             </IonList>  
