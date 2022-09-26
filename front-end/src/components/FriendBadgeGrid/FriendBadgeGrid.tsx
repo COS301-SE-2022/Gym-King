b6daca267d9b3d5cdcbf497fe406e6b59a6c47e6
jademichellepeche <jademichellepeche@gmail.com>
2022-09-26 18:13:36 +0200

    Added view for friends badges

--- front-end/src/components/FriendBadgeGrid/FriendBadgeGrid.tsx
+++ front-end/src/components/FriendBadgeGrid/FriendBadgeGrid.tsx
@@ -11,7 +11,7 @@ export const FriendBadgeGrid=(prop:{badges:any[]})=>{
             <IonRow  className="ion-align-items-center">
                 {prop.badges.map(el => 
                     <IonCol className="center" key={el.id}>
-                    <MyBadgeCard id={el.id} name={el.name} qty={el.qty} badgeEmblem={el.b_id.badgeicon[0]} badgeRank={el.icon[0]}></MyBadgeCard>
+                    <MyBadgeCard id={el.b_id.b_id} name={el.b_id.badgename} qty={el.count} badgeEmblem={el.b_id.badgeicon.split('_')[1]} badgeRank={el.b_id.badgeicon.split('_')[0]}></MyBadgeCard>
                     </IonCol>)}
             </IonRow>
         </IonGrid>
