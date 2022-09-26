3fc71154da54d07d37f3d0b4531a34b0ca61ffc0
Dylan Whiteford <whiteford.dyl@gmail.com>
2022-09-26 10:44:03 +0200

    🐛 fixing build bug

--- front-end/src/components/AcceptRejectCard/AcceptRejectCard.test.tsx
+++ front-end/src/components/AcceptRejectCard/AcceptRejectCard.test.tsx
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react';
-import ActivityList from '../ActivityList/ActivityList';
 import AcceptRejectCard from './AcceptRejectCard'
 
 
--- front-end/src/components/Register/Contact.test.tsx
+++ front-end/src/components/Register/Contact.test.tsx
@@ -2,12 +2,12 @@ import { fireEvent, render, screen} from '@testing-library/react';
 import Contact from './Contact';
 
 test('renders without crashing', () => {
-  const {baseElement} = render(<Contact/>);
+  const {baseElement} = render(<Contact handleChange="" next="" prev=""/>);
   expect(baseElement).toBeDefined();
 });
 
 test('correctly displays labels', async () => {
-  const {baseElement} = render(<Contact/>);
+  const {baseElement} = render(<Contact handleChange="" next="" prev=""/>);
   expect(baseElement).toHaveTextContent("Email*");
   expect(baseElement).toHaveTextContent("Phone Number*");
 });
