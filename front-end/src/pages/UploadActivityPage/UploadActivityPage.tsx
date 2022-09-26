8d6534cc1749b4a7ac671ed7b1ff1bee3e3ae911
Trunks29 <u17169811@tuks.co.za>
2022-09-26 20:16:51 +0200

    syncing branch

--- front-end/src/pages/UploadActivityPage/UploadActivityPage.tsx
+++ front-end/src/pages/UploadActivityPage/UploadActivityPage.tsx
@@ -1,30 +1,20 @@
 import {IonContent, IonText, IonPage, IonHeader, IonGrid, IonRow, IonButton, IonToast, IonLoading, useIonViewDidEnter, IonCol} from '@ionic/react'
-import React, {  useEffect, useRef, useState } from 'react';
+import React, {  useRef, useState } from 'react';
 import { ToolBar } from '../../components/toolbar/Toolbar';
 import './UploadActivityPage.css';
-import {ActivityInputs} from '../../components/activityInputs/ActivityInputs';
-import {claimSchema} from '../../validation/UploadClaimValidation'
 import { useHistory } from 'react-router-dom';
 import BadgeImage from '../../components/BadgeImage/BadgeImage';
 import axios from "axios";
 import * as tf from "@tensorflow/tfjs"
-import {Camera,CameraResultType,CameraSource,Photo} from '@capacitor/camera'
-import { Directory, Filesystem, WriteFileResult} from '@capacitor/filesystem';
+import { Directory, Filesystem} from '@capacitor/filesystem';
 import { Capacitor, Plugins } from '@capacitor/core';
-import { VideoRecorderCamera, VideoRecorderPreviewFrame } from '@teamhive/capacitor-video-recorder';
-import {MediaFile, VideoCapturePlusOptions, VideoCapturePlus,} from "@ionic-native/video-capture-plus";
-import { boolean } from 'yup/lib/locale';
+//import{MediaFile,VideoCapturePlusOptions,VideoCapturePlus} from "@ionic-native/video-capture-plus"
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+//port {DirectoryEntry, File} from "@ionic-native/file"
+import * as mc from "@whiteguru/capacitor-plugin-media-capture"
 import NNAlert from '../../components/NN_outcome/NN_outcome';
-interface LocalFile{
-    name:string;
-    path:string;
-    data:string;
-}
-interface InternalValues {
-    file: any;
-}
-const IMAGE_DIR='GymKing-media'
-export type UploadActivityStates = {act?:any}
+import { VideoRecorderCamera, VideoRecorderPreviewFrame } from '@teamhive/capacitor-video-recorder';
+import '@teamhive/capacitor-video-recorder';
 const config: VideoRecorderPreviewFrame = {
     id: 'video-record',
     stackPosition: 'front', // 'front' overlays your app', 'back' places behind your app.
@@ -34,12 +24,30 @@ const config: VideoRecorderPreviewFrame = {
     y: 0,
     borderRadius: 0
 };
+
+interface InternalValues {
+    file: any;
+}
+const IMAGE_DIR='GymKing-media'
+export type UploadActivityStates = {act?:any}
+
 let categories=['BenchPress_down','BenchPress_up', 'PullUp_down', 'PullUp_up',  'PushUp_down',  'PushUp_up','SitUp_up', 'SitUp_down']
 const UploadActivityPage: React.FC = () =>{
     
-    const { VideoRecorder } = Plugins;
+    const handleTakeVideo=async()=>{
+      /*  const mediaCaptureOptions:mc.CaptureVideoOptions={
+            duration:60,
+            quality:'sd',
+        }
+        
+        await mc.MediaCapture.captureVideo(mediaCaptureOptions).then((mediafile:mc.MediaFileResult) =>{
 
-    const doMediaCapture = async () => {
+            console.log(mediafile)
+        }, error => console.log(error));
+
+*/
+        
+       const { VideoRecorder } = Plugins;
         await VideoRecorder.initialize({
             camera: VideoRecorderCamera.FRONT, // Can use BACK
             previewFrames: [config]
@@ -47,8 +55,24 @@ const UploadActivityPage: React.FC = () =>{
         VideoRecorder.startRecording();
         const res = await VideoRecorder.stopRecording();
         // The video url is the local file path location of the video output.
-            return res.videoUrl;
-      };
+        return res.videoUrl;
+        
+       /* let options:VideoCapturePlusOptions={
+            limit:1,
+            duration:60,
+            highquality:true
+        }
+        await VideoCapturePlus.captureVideo(options).then((mediafile: MediaFile[]) =>{
+
+            console.log(mediafile)
+        }, error => console.log('Something went wrong'));
+
+        //convert to blob
+      /*  return File.readAsArrayBuffer(resolvedPath.nativeURL, media.name).then((buffer)=>{
+            let videoblob=new Blob([buffer],{type: media.type})
+            console.log(videoblob)
+        },(error:any)=>{console.log("video error",error)})*/
+    }
       const [model,setModel]=useState<any>()  
       const [message,setMessage]=useState<string>("loading")
       const loadModel =async() => {
@@ -76,8 +100,7 @@ const UploadActivityPage: React.FC = () =>{
     }
     const [Alert,setAlert]=useState<boolean>(false)
     // STATES AND VARIABLES 
-    const [isValid, setIsValid] = useState(false);
-    const [submitted, setSubmitted] = useState(false);
+
     const [Icon,setIcon]=useState<string[]>(["b","cycle"])
     let email = localStorage.getItem("email") 
     localStorage.setItem( 'e1', "");
@@ -92,55 +115,6 @@ const UploadActivityPage: React.FC = () =>{
     const [username, setUsername]=useState("");
     let history=useHistory()
         
-        //METHODS 
-        const selectImage=async()=>{
-            const image=await Camera.getPhoto({
-                quality:90,
-                allowEditing:true,
-                resultType:CameraResultType.Base64,
-                source:CameraSource.Camera
-            })
-            console.log(image)
-            if(image){
-                saveImage(image)
-            }
-            
-        }
-        const saveImage=async(media:Photo)=>{
-            const fileName=new Date().getTime()+'.mp4'
-            const base64Data=await ToBase64(media) as string
-            const savedFile=await Filesystem.writeFile({
-                directory:Directory.Data,
-                path: `${IMAGE_DIR}/${fileName}`,
-                data:base64Data
-            })
-            console.log('saved:',savedFile)
-        }
-        const ToBase64=async(photo:Photo)=>
-        {
-            if (Capacitor.getPlatform()==="hybrid") {
-                const file = await Filesystem.readFile({
-                  path: String(photo.path)
-                });
-                return file.data;
-              }
-              else {
-                // Fetch the photo, read as a blob, then convert to base64 format
-                const response = await fetch(String(photo.webPath));
-                const blob = await response.blob();
-            
-                return await convertBlobToBase64(blob) as string;
-              }
-        }
-       const convertBlobToBase64 = (blob:Blob) => new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onerror = reject;
-            reader.onload = () => {
-                resolve(reader.result);
-            };
-            reader.readAsDataURL(blob);
-        });
-
     const categroize=async(images:ImageData[])=>{
        let predictions=[]
        for(let i=0;i<images.length;i++)
@@ -199,13 +173,14 @@ const UploadActivityPage: React.FC = () =>{
            }
        
     }
-    const handleSubmit = async (e:any) =>{
+    const handleSubmit = async (path:any) =>{
+      //  await sendClaim();
        // saveImage(values.current.file)
-        console.log("model:",model)
+       console.log("model:",model)
         setMessage("Calculating")
         setLoading(true)
         console.log("extracting frames")
-        await VideoToFrames.getFrames('./assets/video.mp4', VideoToFramesMethod.totalFrames).then(async function (frame:ImageData[]) {
+        await VideoToFrames.getFrames("https://storage.googleapis.com/gymkingfiles.appspot.com/claims%2F1664197886149.mp4", VideoToFramesMethod.totalFrames).then(async function (frame:ImageData[]) {
             console.log("running through neural network")
             console.log(frame)
             let predictions=await categroize(frame)
@@ -214,33 +189,8 @@ const UploadActivityPage: React.FC = () =>{
         console.log("done")
     
         setLoading(false)
-        setAlert(true)
-       // setAlert(true)
-    
-           /*e.preventDefault();
-            formdata={
-                i1: e.target.i1.value,
-                i2: e.target.i2.value,
-                i3: e.target.i3.value,
-            };
-            //console.log(formData);
-
-            if(formdata.i1 == null)
-                localStorage.setItem( 'e1', "This field is required");
-            if(formdata.i2 == null)
-                localStorage.setItem( 'e2', "This field is required");
-            if(formdata.i3 == null)
-                localStorage.setItem( 'e3', "This field is required");
-
-            const isValid = await claimSchema.isValid(formdata);
-            setSubmitted(true);
-            if(isValid)
-            {
-                setIsValid(true);
-                //handle post request 
-                sendClaim();
-               
-            }*/
+        setAlert(true)  
+           
             
         }
         
@@ -250,14 +200,15 @@ const UploadActivityPage: React.FC = () =>{
            
         
         // GET BADGES GET REQUEST 
-        useIonViewDidEnter(()=>{
-            loadModel()
-            /*let badgeId= sessionStorage.getItem("badgeid");
+        useIonViewDidEnter(async()=>{
+        
+            await loadModel()
+          /*  let badgeId= sessionStorage.getItem("badgeid");
             setLoading(true)
             axios.get(process.env["REACT_APP_GYM_KING_API"]+`/badges/badge/${badgeId}`)
-            .then(response =>response.data)
-            .then(response =>{
-                //console.log("rsponse",response)
+                .then(response =>response.data)
+                .then(response =>{
+                console.log("rsponse",response)
                 setB_id(response.b_id)
                 localStorage.setItem("activitytype", response.activitytype)
                 setDescription(response.badgechallenge)
@@ -291,8 +242,8 @@ const UploadActivityPage: React.FC = () =>{
             .catch(err => {
                 console.log(err)
                 setLoading(false)
-            })
-        */})
+            })*/
+        })
         const values =useRef<InternalValues>({
             file: false,
         });
@@ -305,18 +256,16 @@ const UploadActivityPage: React.FC = () =>{
                 return false;
             }    
         }
-        // SEND CLAIM POST REQUEST 
-       /* const sendClaim=()=>{
-            let i1= formdata.i1;
-            let i2= formdata.i2;
-            let i3= formdata.i3;
+    
+        const sendClaim=async()=>{
+            console.log(values.current.file)
             let formData = new FormData();
                 formData.append("bid", b_id)
                 formData.append("email", email!)
                 formData.append("password", localStorage.getItem("password")!)
-                formData.append("input1", i1)
-                formData.append("input2", i2)
-                formData.append("input3", i3)
+                formData.append("input1", "")
+                formData.append("input2", "")
+                formData.append("input3", "")
                 formData.append('proof', values.current.file, values.current.file.name);
                 console.log(formData);
             axios(process.env["REACT_APP_GYM_KING_API"]+`/claims/claim`,{
@@ -325,14 +274,58 @@ const UploadActivityPage: React.FC = () =>{
             })
             .then(response =>response.data)
             .then(response =>{
-                //console.log(response);
-                setShowToast1(true);
-                sessionStorage.removeItem("badgeid")
-                history.goBack();
+                console.log(response);
+                
             })
-            .catch(err => {console.log(err)}) 
+            .catch(err => {console.log(err)})
+        } 
+        const selectImage=async()=>{
+            const image=await Camera.getPhoto({
+                quality:90,
+                allowEditing:true,
+                resultType:CameraResultType.Base64,
+                source:CameraSource.Camera
+            })
+            console.log(image)
+            if(image){
+                saveImage(image)
+            }
+            
         }
-    */
+        const saveImage=async(media:Photo)=>{
+            const fileName=new Date().getTime()+'.jpeg'
+            const base64Data=await ToBase64(media) as string
+            const savedFile=await Filesystem.writeFile({
+                directory:Directory.Data,
+                path: `${IMAGE_DIR}/${fileName}`,
+                data:base64Data
+            })
+            console.log('saved:',savedFile)
+        }
+        const ToBase64=async(photo:Photo)=>
+        {
+            if (Capacitor.getPlatform()==="hybrid") {
+                const file = await Filesystem.readFile({
+                  path: String(photo.path)
+                });
+                return file.data;
+              }
+              else {
+                // Fetch the photo, read as a blob, then convert to base64 format
+                const response = await fetch(String(photo.webPath));
+                const blob = await response.blob();
+            
+                return await convertBlobToBase64(blob) as string;
+              }
+        }
+       const convertBlobToBase64 = (blob:Blob) => new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onerror = reject;
+            reader.onload = () => {
+                resolve(reader.result);
+            };
+            reader.readAsDataURL(blob);
+        });
         const reset= () => {
            setAlert(false)
         }
@@ -356,8 +349,11 @@ const UploadActivityPage: React.FC = () =>{
                         </IonRow>
                     </IonGrid>
                     <IonText className='PageTitle center'>{badgename}</IonText>
-                    <IonText className='SmallDescription center'>{badgedescription}</IonText> <br></br>
-                    <IonButton onClick={ handleSubmit} className="btnSubmit centerComp" color="warning">TEST NN</IonButton>
+                   
+                    <IonButton onClick={ handleSubmit} className="btnSubmit centerComp" color="warning">TAKE VIDEO</IonButton>
+                    <IonButton onClick={selectImage} className="btnSubmit centerComp" color="warning">TAKE PICTURE</IonButton>
+                    <input  type="file" accept=".jpg, .png, .avi, .mkv, .asf, .wmv, .mp4, .m4v, .mov, .3gp, .vro, .mpg, .mpeg, .mov" onChange={(ev) => onFileChange(ev)} />
+                    <IonButton onClick={sendClaim} className="btnSubmit centerComp" color="warning">Submit</IonButton>  
                     <IonToast
                         isOpen={showToast1}
                         onDidDismiss={() => setShowToast1(false)}
