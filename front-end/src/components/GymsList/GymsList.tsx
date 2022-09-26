5f8f1ecd0cfc88e052d4f1bf8096c662263e72fe
jademichellepeche <jademichellepeche@gmail.com>
2022-09-26 19:22:16 +0200

    Updated MyGyms

--- front-end/src/components/GymsList/GymsList.tsx
+++ front-end/src/components/GymsList/GymsList.tsx
@@ -1,10 +1,67 @@
-import { IonItem, IonList, IonAvatar, IonImg, IonLabel} from '@ionic/react';
-import React from 'react'
-
+import { IonItem, IonList, IonAvatar, IonImg, IonLabel, useIonViewDidEnter} from '@ionic/react';
+import React, {useState} from 'react'
+import axios from "axios";
 
 export type props = {gymsList?:any}
 
 const GymsList: React.FC<props> = (props) =>{
+
+    let objectArray:Object[]=[]
+    const [gyms, setGyms] = useState(objectArray)
+    useIonViewDidEnter(async()=>{
+        let array: any[]=[]
+        let gymIDs: any[]=[]
+        let gymObjects: any[]=[]
+        await axios(process.env["REACT_APP_GYM_KING_API"]+`/users/user/getGymSubscriptions`,{
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            data: JSON.stringify({ 
+                fromEmail: localStorage.getItem("email"),
+
+            })
+        })
+        .then(response =>response.data)
+        .then(response =>{
+            console.log(response)
+            array = response.results
+        })
+        .catch(err => {
+            console.log(err)
+            
+            
+        })
+        //console.log(array)
+        //extract the gymsIDs from the results 
+        // eslint-disable-next-line 
+        array.map((el)=>{
+            gymIDs.push(el.toGym)
+        })
+        //console.log(gymIDs)
+        //get the gym information for each g_id
+
+        // eslint-disable-next-line 
+        gymIDs.map(async (gid)=>{
+            let gym ={}
+            await axios.get(process.env["REACT_APP_GYM_KING_API"]+`/gyms/gym/${gid}`)
+            .then(response =>response.data)
+            .then(response =>{
+                console.log(response)
+                gym = response
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+            gymObjects.push(gym)
+        })
+        console.log(gymObjects)
+        setGyms(gymObjects)
+        console.log(gymObjects)
+        
+    },[])
 
 
         const viewGymProfile = () =>{
@@ -15,8 +72,8 @@ const GymsList: React.FC<props> = (props) =>{
             
             <IonList>
                 {
-                    props.gymsList.map((el:any)=>{
-                        return (<IonItem button detail  onClick={viewGymProfile} data-testid="aB" key={el.email + Math.random()}>
+                    gyms.map((el:any)=>{
+                        return (<IonItem button detail  onClick={viewGymProfile} data-testid="aB" key={el.g_id + Math.random()}>
                                 <IonAvatar style={{"marginRight":"1em", "marginBottom":"3%"}}>
                                     <IonImg  style={{"position":"absolute","overflow":"hidden","marginTop":"6px","borderRadius":"50%","backgroundImage":`url(${el.profile})`}} alt="" className="toolbarImage  contain "  ></IonImg>                        
                                 </IonAvatar>
--- front-end/src/pages/MyGyms/MyGyms.tsx
+++ front-end/src/pages/MyGyms/MyGyms.tsx
@@ -1,35 +1,11 @@
-import React, {useState} from 'react'
-import {IonContent, IonText, IonPage, IonHeader, useIonViewWillEnter} from '@ionic/react';
+import React from 'react'
+import {IonContent, IonText, IonPage, IonHeader} from '@ionic/react';
 import { ToolBar } from '../../components/toolbar/Toolbar';
 import GymsList from '../../components/GymsList/GymsList';
-import axios from "axios";
 
 const MyGyms: React.FC = () =>{
 
-    const [gyms, setGyms] = useState([])
-    useIonViewWillEnter(()=>{
-        axios(process.env["REACT_APP_GYM_KING_API"]+`/users/user/getGymSubscriptions`,{
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            },
-            data: JSON.stringify({ 
-                fromEmail: localStorage.getItem("email"),
 
-            })
-        })
-        .then(response =>response.data)
-        .then(response =>{
-            console.log(response)
-            setGyms(response.results)
-        })
-        .catch(err => {
-            console.log(err)
-            
-        })
-
-    },[])
     //=================================================================================================
     //    Render
     //=================================================================================================
@@ -41,7 +17,7 @@ const MyGyms: React.FC = () =>{
                 <br></br>
                 <IonContent fullscreen className='Content'>
                     <IonText className='PageTitle center'>My Gyms</IonText>
-                    <GymsList gymsList={gyms}></GymsList>
+                    <GymsList ></GymsList>
                     
                 </IonContent>
             </IonPage>
