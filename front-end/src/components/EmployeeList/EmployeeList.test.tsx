a26d0f858d4030b0da02f5c842ff55d9ec71b85d
jademichellepeche <jademichellepeche@gmail.com>
2022-09-27 11:07:42 +0200

    Fixed some memory leaks

--- front-end/src/components/EmployeeList/EmployeeList.test.tsx
+++ front-end/src/components/EmployeeList/EmployeeList.test.tsx
@@ -1,9 +1,6 @@
 import { render} from '@testing-library/react';
 import EmployeeList from './EmployeeList';
 
-////TESTS TO BE PERFORMED////
-/*
-*/
 
 test('renders without crashing', () => {
   const {baseElement} = render(<EmployeeList list={[]} history=""/>);
--- front-end/src/pages/MyBadge/MyBadge.test.tsx
+++ front-end/src/pages/MyBadge/MyBadge.test.tsx
@@ -1,12 +1,13 @@
-import {render} from '@testing-library/react';
+
+import { render} from '@testing-library/react';
 import MyBadge from './MyBadge';
 
 ////TESTS TO BE PERFORMED////
 /*
 */
 
-test('renders without crashing', () => {
-  const { baseElement } = render(<MyBadge/>);
+test('renders without crashing', async() => {
+  const { baseElement } = await render(<MyBadge/>);
   expect(baseElement).toBeDefined();
 });
 
@@ -17,7 +18,7 @@ describe('Testing connection to api', () => {
 
   it('should load user badges', async () => {
       (async ()=>{
-        fetch(process.env["REACT_APP_GYM_KING_API"]+`/users/owned/${"user_email"}`,{
+        await fetch(process.env["REACT_APP_GYM_KING_API"]+`/users/owned/${"user_email"}`,{
             "method":"GET"
         })
         .then(response =>response.json())
