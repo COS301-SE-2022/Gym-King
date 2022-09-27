61511fae05ff849f996367175ec0e4b246d335d2
jademichellepeche <jademichellepeche@gmail.com>
2022-09-27 13:43:42 +0200

    Update Identifications.test.tsx

--- front-end/src/components/Register/Identifications.test.tsx
+++ front-end/src/components/Register/Identifications.test.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import Identifications from './Identifications';
 
 test('renders without crashing', async() => {
-  const{baseElement} = render(<Identifications handleChange={undefined} next={undefined} history={undefined}/>);
+  const{baseElement} = await render(<Identifications handleChange={undefined} next={undefined} history={undefined}/>);
   expect (baseElement).toBeDefined();
 });
 
 test('correctly displays labels', async () => {
-  const {baseElement} = render(<Identifications handleChange={undefined} next={undefined} history={undefined}/>);
+  const {baseElement} = await render(<Identifications handleChange={undefined} next={undefined} history={undefined}/>);
   expect (baseElement).toHaveTextContent("Full Name*");
   expect (baseElement).toHaveTextContent("Username*");
 });
