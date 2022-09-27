ef32a197e7bcda5e2b50dee25e76ea1267cef12f
jademichellepeche <jademichellepeche@gmail.com>
2022-09-27 13:23:32 +0200

    Update EmployeeList.test.tsx

--- front-end/src/components/EmployeeList/EmployeeList.test.tsx
+++ front-end/src/components/EmployeeList/EmployeeList.test.tsx
@@ -1,10 +1,8 @@
 import { render} from '@testing-library/react';
 import EmployeeList from './EmployeeList';
-import { useHistory } from 'react-router-dom';
 
 
 test('renders without crashing',async () => {
-  let history=useHistory()
-  const {baseElement} = await render(<EmployeeList list={[]} history={history}/>);
+  const {baseElement} = await render(<EmployeeList list={[]} history={undefined}/>);
   expect(baseElement).toBeDefined();
 });
