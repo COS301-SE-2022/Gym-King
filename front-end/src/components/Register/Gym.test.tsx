569ce3b10882bcc04f0e8476484438d8286c023d
jademichellepeche <jademichellepeche@gmail.com>
2022-09-27 13:27:05 +0200

    Update Gym.test.tsx

--- front-end/src/components/Register/Gym.test.tsx
+++ front-end/src/components/Register/Gym.test.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import Gym from './Gym';
 
 test('renders without crashing', async() => {
-  const{baseElement} = render(<Gym handleChange={act(()=>{})} next={act(()=>{})} prev={act(()=>{})}/>);
+  const{baseElement} = await render(<Gym handleChange={act(()=>{return})} next={act(()=>{return})} prev={act(()=>{return})}/>);
   expect(baseElement).toBeDefined();
 });
 
 test('correctly displays labels', async () => {
-  const {baseElement} = render(<Gym handleChange={act(()=>{})} next={act(()=>{})} prev={act(()=>{})} />);
+  const {baseElement} = render(<Gym handleChange={act(()=>{return})} next={act(()=>{return})} prev={act(()=>{return})} />);
   expect (baseElement).toHaveTextContent("Please select your gym*");
 });
