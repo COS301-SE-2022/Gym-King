8db720d47ffcd578a6168ec942dbf8eb489716f1
jademichellepeche <jademichellepeche@gmail.com>
2022-09-27 14:18:49 +0200

    Update MyBadgeCard.test.tsx

--- front-end/src/components/MyBadgeGrid/MyBadgeCard/MyBadgeCard.test.tsx
+++ front-end/src/components/MyBadgeGrid/MyBadgeCard/MyBadgeCard.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { MyBadgeCard } from './MyBadgeCard'
 
 test('renders without crashing',async() => {
-    const { baseElement } =await render(<MyBadgeCard id="" name="" qty={0} badgeEmblem="" badgeRank='' />);
+    const { baseElement } =await render(<MyBadgeCard id={null} name="" qty={0} badgeEmblem="" badgeRank="" />);
     expect(baseElement).toBeDefined();
   });
 
