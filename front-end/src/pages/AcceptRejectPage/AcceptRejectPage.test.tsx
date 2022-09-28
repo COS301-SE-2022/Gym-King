652f78faba8de32915f84d734ca01914241817d1
jademichellepeche <jademichellepeche@gmail.com>
2022-09-28 14:40:21 +0200

    Update AcceptRejectPage.test.tsx

--- front-end/src/pages/AcceptRejectPage/AcceptRejectPage.test.tsx
+++ front-end/src/pages/AcceptRejectPage/AcceptRejectPage.test.tsx
@@ -32,7 +32,7 @@ describe('Testing connection to api', () => {
 
     it('should load claim data',  () => {
         let test = (async()=>{
-            await fetch(process.env["REACT_APP_GYM_KING_API"]+`/claims/claim?bid=${badgeId}&email=${email}`,{
+            await fetch(process.env["REACT_APP_GYM_KING_API"]+`/claims/claim?bid=${badgeId}&username=${localStorage.getItem("username")!}`,{
                 "method":"GET"
             })
             .then(response =>response.json())
