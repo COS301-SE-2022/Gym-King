3bd15a436867e0c3c8185399a8ad058e21890acd
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-09-29 13:01:50 +0200

    Update FriendsPage.test.tsx

--- front-end/src/pages/FriendsPage/FriendsPage.test.tsx
+++ front-end/src/pages/FriendsPage/FriendsPage.test.tsx
@@ -2,6 +2,7 @@ jest.setTimeout(250000)
 import { render } from '@testing-library/react';
 import FriendsPage from './FriendsPage';
 
+//tests to see if the page renders without crashing
 test('renders without crashing', async () => {
     const { baseElement } =await  render(<FriendsPage />);
     expect(baseElement).toBeDefined();
