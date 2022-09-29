c9566ce2221d0f76af7b946bfe738bb0a47cd7db
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-09-29 13:02:32 +0200

    Update FriendProfile.test.tsx

--- front-end/src/pages/FriendProfile/FriendProfile.test.tsx
+++ front-end/src/pages/FriendProfile/FriendProfile.test.tsx
@@ -2,6 +2,7 @@ jest.setTimeout(250000)
 import { render } from '@testing-library/react';
 import FriendProfile from './FriendProfile';
 
+//Tests to see if the FriendProfile page renders without crashing
 test('renders without crashing', async () => {
     const { baseElement } =await  render(<FriendProfile />);
     expect(baseElement).toBeDefined();
