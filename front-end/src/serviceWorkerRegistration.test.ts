eaf6f092e96a590d6dd2a89b98f6bb7732d8a07b
jademichellepeche <jademichellepeche@gmail.com>
2022-09-29 12:13:56 +0200

    fix

--- front-end/src/serviceWorkerRegistration.test.ts
+++ front-end/src/serviceWorkerRegistration.test.ts
@@ -1,14 +1,17 @@
-jest.setTimeout(25000)
 
-import {register} from './serviceWorkerRegistration'
+import {register, unregister, registerValidSW, checkValidServiceWorker} from './serviceWorkerRegistration'
 
 type Config = {
     onSuccess?: (registration: ServiceWorkerRegistration) => void;
     onUpdate?: (registration: ServiceWorkerRegistration) => void;
   };
+
+
 test('renders without crashing', async() => {
     let config :Config
 
     expect(() => register(config)).not.toThrow();
-
+    expect(() => unregister()).not.toThrow();
+    expect(() => registerValidSW).not.toThrow();
+    expect(() => checkValidServiceWorker).not.toThrow();
 });
