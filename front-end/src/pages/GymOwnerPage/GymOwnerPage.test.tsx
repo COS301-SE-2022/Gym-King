5ae9bb5d2e40c784d430a297b1baeabc9dd29b50
Robyn C. Hancock <92938217+RobynHancock@users.noreply.github.com>
2022-09-29 13:09:26 +0200

    Update GymOwnerPage.test.tsx

--- front-end/src/pages/GymOwnerPage/GymOwnerPage.test.tsx
+++ front-end/src/pages/GymOwnerPage/GymOwnerPage.test.tsx
@@ -2,7 +2,7 @@ jest.setTimeout(25000)
 import { render} from '@testing-library/react';
 import GymOwnerPage from './GymOwnerPage';
 
-
+//tests to see if the GymOwner Page renders without crashing
 test('renders without crashing', async() => {
   const {baseElement} =await render(<GymOwnerPage />);
   expect(baseElement).toBeDefined();
